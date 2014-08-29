76a2a34d20df4b4351da57be0970826bd123296d
jooheeh114 <jooheeh114@gmail.com>
2014-08-28 23:00:49 -0700

    fixed error data

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ event.preventDefault();
 var firstname, lastname;
 firstname = $("#firstname").val();
 lastname = $("#lastname").val();
-$.get("http://joohee.herokuapp.com?fn="+firstname+"&ln="+lastname, function(error,data){
+$.get("http://joohee.herokuapp.com?fn="+firstname+"&ln="+lastname, function(data, error){
 	$("#results").html(data);
 
 
